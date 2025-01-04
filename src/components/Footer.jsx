b02538e5f16fe1696dfd2d48b93ddbe9c1a76a4e
bootaan23 <cabdirisaaqbootaan1@gmail.com>
2025-01-04 19:24:59 +0300

    updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div className='max-w-[1300px] mx-auto flex justify-between p-6 md:p-20 text-sm md:text-lg mt-12'>
         <div className='space-y-4'>
-            <h3 className='text-2xl text-gray-200 font-semibold'>Abdirizak Botan .</h3>
+            <h3 className='text-2xl text-gray-200 font-semibold'>Abdirizak Botan</h3>
             <div className='flex flex-row gap-6 text-gray-400 text-4xl'>
                 <a href="https://github.com/AbdirizakBotan"><FaGithubSquare /></a>
                 <a href="#"><FaInstagram /></a>
